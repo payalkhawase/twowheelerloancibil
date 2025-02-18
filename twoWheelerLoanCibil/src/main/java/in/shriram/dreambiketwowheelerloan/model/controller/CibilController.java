7e61d59f42182a484233e66ad0a9b4be2f06d324
payalkhawase <payal.khawase14@gmail.com>
2025-02-18 22:26:03 +0530

    add autowired annotation

--- twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/controller/CibilController.java
+++ twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/controller/CibilController.java
@@ -1,8 +1,13 @@
 package in.shriram.dreambiketwowheelerloan.model.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import in.shriram.dreambiketwowheelerloan.model.service.CibilService;
 
 @RestController
 public class CibilController {
 
+	@Autowired
+	CibilService cs;
 }
--- twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/serviceimpl/CibilServiceImpl.java
+++ twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/serviceimpl/CibilServiceImpl.java
@@ -1,10 +1,15 @@
 package in.shriram.dreambiketwowheelerloan.model.serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import in.shriram.dreambiketwowheelerloan.model.repository.CibilRepo;
 import in.shriram.dreambiketwowheelerloan.model.service.CibilService;
 
 @Service
 public class CibilServiceImpl implements CibilService{
 
+	@Autowired
+	CibilRepo cr;
+	
 }
