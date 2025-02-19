afd3756ee9acd42917f2480491d5a65b93f71742
payalkhawase <payal.khawase14@gmail.com>
2025-02-19 08:57:46 +0530

    add annotation in pojo class

--- twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/model/Cibil.java
+++ twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/model/Cibil.java
@@ -3,12 +3,17 @@ package in.shriram.dreambiketwowheelerloan.model.model;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Cibil {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int cibilId;
 	private int cibilScore;
 	private Date cibilscoredDateTime;
