e667e5028653040bf80edddddf33b3373d049909
sujata-cell <srjp90@gmail.com>
2025-02-19 07:03:39 +0530

    added @Id in cibil model and added put mapping method to update cibil status

--- twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/service/CibilService.java
+++ twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/service/CibilService.java
@@ -1,5 +1,9 @@
 package in.shriram.dreambiketwowheelerloan.model.service;
 
+import in.shriram.dreambiketwowheelerloan.model.model.Cibil;
+
 public interface CibilService {
+
+	public Cibil updateCibilStatus(int cibilId, String status);
 
 }
