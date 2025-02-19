cdc290aebea90eb50031f3f7fd0ea2f5faf32bba
priyankabagde2610 <priyankabagde2610@gmail.com>
2025-02-19 16:35:52 +0530

    add something

--- twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/service/CibilService.java
+++ twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/service/CibilService.java
@@ -1,6 +1,10 @@
 package in.shriram.dreambiketwowheelerloan.model.service;
 
+import in.shriram.dreambiketwowheelerloan.model.model.Cibil;
+
 public interface CibilService {
+
+	Cibil updateCibilStatus(int cibilId, String status);
 
 }
  
--- twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/serviceimpl/CibilServiceImpl.java
+++ twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/serviceimpl/CibilServiceImpl.java
@@ -3,6 +3,7 @@ package in.shriram.dreambiketwowheelerloan.model.serviceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import in.shriram.dreambiketwowheelerloan.model.model.Cibil;
 import in.shriram.dreambiketwowheelerloan.model.repository.CibilRepo;
 import in.shriram.dreambiketwowheelerloan.model.service.CibilService;
 
