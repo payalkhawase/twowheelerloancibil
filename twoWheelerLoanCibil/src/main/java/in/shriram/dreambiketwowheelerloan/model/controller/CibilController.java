06efc6175578c48613b8e19c9ae5214f1623108e
GajananChavhan-pixel <shubhamchavhan077@gmail.com>
2025-02-21 11:45:36 +0530

    added space

--- twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/controller/CibilController.java
+++ twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/controller/CibilController.java
@@ -4,9 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.GetMapping;
-=======
 
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import in.shriram.dreambiketwowheelerloan.model.model.Cibil;
-import in.shriram.dreambiketwowheelerloan.model.model.Enquiry;
 import in.shriram.dreambiketwowheelerloan.model.service.CibilService;
 import jakarta.validation.Valid;
 
