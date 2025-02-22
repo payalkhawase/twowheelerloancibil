5e63667a87f4af02f1c3f2d1da039d679cdf9976
payalkhawase <payal.khawase14@gmail.com>
2025-02-22 10:25:35 +0530

    add space

--- twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/controller/CibilController.java
+++ twoWheelerLoanCibil/src/main/java/in/shriram/dreambiketwowheelerloan/model/controller/CibilController.java
@@ -1,13 +1,9 @@
 package in.shriram.dreambiketwowheelerloan.model.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
